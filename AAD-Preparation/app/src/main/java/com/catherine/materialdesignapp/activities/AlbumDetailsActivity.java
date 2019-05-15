56c4086cce0a0c8bc154c6866cf3e9d0d754c8f2
Catherine Chen <catherine_chen@trendmicro.com>
2019-05-15 15:05:24 +0800

    [Android] backup

--- AAD-Preparation/app/src/main/java/com/catherine/materialdesignapp/activities/AlbumDetailsActivity.java
+++ AAD-Preparation/app/src/main/java/com/catherine/materialdesignapp/activities/AlbumDetailsActivity.java
@@ -6,21 +6,38 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.catherine.materialdesignapp.R;
+import com.catherine.materialdesignapp.adapters.AddToPlaylistAdapter;
 import com.catherine.materialdesignapp.adapters.SongAdapter;
-import com.catherine.materialdesignapp.fragments.PlaylistFragment;
 import com.catherine.materialdesignapp.listeners.OnItemClickListener;
+import com.catherine.materialdesignapp.models.Playlist;
+import com.catherine.materialdesignapp.models.Song;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 public class AlbumDetailsActivity extends BaseActivity {
     private final static String TAG = AlbumDetailsActivity.class.getSimpleName();
-    private ConstraintLayout container;
+    private BottomSheetDialog bottomSheetDialog;
+    private AddToPlaylistAdapter addToPlaylistAdapter;
+    private String albumName, artist;
+    private DatabaseReference myRef;
+    private List<Playlist> playlists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +56,8 @@ public class AlbumDetailsActivity extends BaseActivity {
 
         Bundle bundle = getIntent().getExtras();
         Uri coverUri = Uri.parse(bundle.getString("cover"));
-        String albumName = bundle.getString("album");
-        String artist = bundle.getString("artist");
+        albumName = bundle.getString("album");
+        artist = bundle.getString("artist");
         String[] songs = bundle.getStringArray("songs");
 
         SimpleDraweeView sdv_cover = findViewById(R.id.sdv_cover);
@@ -60,7 +77,9 @@ public class AlbumDetailsActivity extends BaseActivity {
             @Override
             public void onItemClick(View view, int position) {
                 Log.d(TAG, "clicked:" + position);
-                showFragment();
+//                showFragment();
+                bottomSheetDialog.show();
+
             }
 
             @Override
@@ -69,24 +88,90 @@ public class AlbumDetailsActivity extends BaseActivity {
             }
         });
         recyclerView.setAdapter(adapter);
-        container = findViewById(R.id.container);
+
+        // bottom sheet
+        bottomSheetDialog = new BottomSheetDialog(this);
+        View bottomSheet = getLayoutInflater().inflate(R.layout.bottom_sheet_add_to_playlist, null);
+        bottomSheetDialog.setContentView(bottomSheet);
+
+        RecyclerView rv_playlists = bottomSheetDialog.findViewById(R.id.rv_playlists);
+        rv_playlists.setLayoutManager(new LinearLayoutManager(this));
+        addToPlaylistAdapter = new AddToPlaylistAdapter(this, null, new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+                if (position != addToPlaylistAdapter.getItemCount() - 1) {
+                    addToPlaylist(playlists.get(position), addToPlaylistAdapter.getEntity(position));
+                } else {
+                    //create new playlist
+                }
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+
+            }
+        });
+        rv_playlists.setAdapter(addToPlaylistAdapter);
+        getPlaylists();
     }
 
-    @Override
-    public void onBackPressed() {
-        if (getSupportFragmentManager().getBackStackEntryCount() == 0)
-            ActivityCompat.finishAfterTransition(this);
-        else {
-            getSupportFragmentManager().popBackStack();
-            container.setVisibility(View.VISIBLE);
+    private void getPlaylists() {
+        String DB_PATH = "test_playlist"; // TODO playlists
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference(DB_PATH);
+        ValueEventListener firebaseValueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Log.d(TAG, String.format("size: %d", dataSnapshot.getChildrenCount()));
+                playlists = new ArrayList<>();
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    Playlist playlist = child.getValue(Playlist.class);
+                    Log.i(TAG, String.format("%s: %s", child.getKey(), playlist));
+                    playlists.add(playlist);
+                }
+                Collections.sort(playlists);
+                addToPlaylistAdapter.setEntities(fillInTitles(playlists));
+                addToPlaylistAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+                addToPlaylistAdapter.setEntities(fillInTitles(null));
+                addToPlaylistAdapter.notifyDataSetChanged();
+            }
+        };
+        myRef.addValueEventListener(firebaseValueEventListener);
+    }
+
+    private String[] fillInTitles(List<Playlist> playlists) {
+        String defaultString = getResources().getString(R.string.add_new_playlist);
+        if (playlists == null || playlists.size() == 0) {
+            return new String[]{defaultString};
+        } else {
+            String[] titles = new String[playlists.size() + 1];
+            for (int i = 0; i < playlists.size(); i++) {
+                titles[i] = playlists.get(i).getName();
+            }
+            titles[titles.length - 1] = defaultString;
+            return titles;
         }
     }
 
-    private void showFragment() {
-        container.setVisibility(View.INVISIBLE);
-        getSupportFragmentManager().beginTransaction()
-                .addToBackStack("bottom_sheet")
-                .replace(R.id.fl_bottom_fragment, new PlaylistFragment(), "bottom_sheet")
-                .commit();
+    private void addToPlaylist(Playlist playlist, String songName) {
+        Song song = new Song();
+        song.setAlbum(albumName);
+        song.setArtist(artist);
+        Map<String, Song> songs = playlist.getSongs();
+        songs.put(songName, song);
+
+        myRef.child(playlist.getName()).child("songs").setValue(songs, (databaseError, databaseReference) -> {
+            if (databaseError != null)
+                Log.e(TAG, databaseError.getMessage());
+        });
     }
 }
---
+++ AAD-Preparation/app/src/main/java/com/catherine/materialdesignapp/adapters/AddToPlaylistAdapter.java
@@ -0,0 +1,79 @@
+package com.catherine.materialdesignapp.adapters;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.catherine.materialdesignapp.R;
+import com.catherine.materialdesignapp.listeners.OnItemClickListener;
+
+public class AddToPlaylistAdapter extends RecyclerView.Adapter<AddToPlaylistAdapter.MainRvHolder> {
+    private final String TAG = AddToPlaylistAdapter.class.getSimpleName();
+    private Context ctx;
+    private String[] entities;
+    private OnItemClickListener listener;
+
+    public AddToPlaylistAdapter(Context ctx, String[] entities, OnItemClickListener listener) {
+        this.ctx = ctx;
+        this.listener = listener;
+        this.entities = entities;
+    }
+
+    @NonNull
+    @Override
+    public MainRvHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new MainRvHolder(LayoutInflater.from(ctx).inflate(R.layout.rv_add_to_playlist_item, viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MainRvHolder mainRvHolder, final int position) {
+        if (entities == null || entities.length == 0)
+            return;
+
+        if (listener != null) {
+            mainRvHolder.itemView.setOnClickListener(v -> listener.onItemClick(mainRvHolder.itemView, position));
+        }
+
+        String song = entities[position];
+        if (!TextUtils.isEmpty(song)) {
+            mainRvHolder.tv_title.setVisibility(View.VISIBLE);
+            mainRvHolder.tv_title.setText(song);
+        } else
+            mainRvHolder.tv_title.setVisibility(View.GONE);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return entities.length;
+    }
+
+    public void setEntities(String[] entities) {
+        this.entities = entities;
+    }
+
+    public String[] getEntities() {
+        return this.entities;
+    }
+
+    public String getEntity(int position) {
+        return entities[position];
+    }
+
+    class MainRvHolder extends RecyclerView.ViewHolder {
+        TextView tv_title;
+        View itemView;
+
+        MainRvHolder(View itemView) {
+            super(itemView);
+            tv_title = itemView.findViewById(R.id.tv_title);
+            this.itemView = itemView;
+        }
+    }
+}
